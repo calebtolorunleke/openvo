89c2830af096a253be4b58999d532f77fafb2c80
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-27 23:47:02 -0700

    initial commit on the openvo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
+  const name = "Caleb";
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <h1>Hello World, I'm {name} </h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
