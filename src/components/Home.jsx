dbf7405b7cffbb0b1166e0b34a4a002736cffce8
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-28 01:12:09 -0700

    feat: added a global state management feature

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>You are not logged in. Go to the login page to sign in</p>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div className="text-white">Login</div>;
+  const [name, setName] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    alert("Login");
+  };
+  return (
+    <div>
+      <h1 className="text-4xl font-semibold pb-5">Login</h1>
+      <form className="text-white flex gap-5">
+        <label htmlFor="">Name</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="bg-gray-400"
+        />
+        <button className="bg-gray-600 p-1 rounded px-3" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
