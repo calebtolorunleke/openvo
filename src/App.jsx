7763977b1a98421ebf474cc0c56191e46227a0e0
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-28 00:34:03 -0700

    initial commit on the openvo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route element={<Home />} path="/" />
       </Routes>
     </BrowserRouter>
   );
