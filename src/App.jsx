eeda21a4bdc550680cb4e16cf7350753c7be3cc9
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-28 01:03:17 -0700

    feat: added a global state management feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import Notfound from "./components/Notfound";
+import { Navbar } from "./components/Navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />{" "}
       <Routes>
         <Route element={<Home />} path="/" />
+        <Route element={<About />} path="/about" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Notfound />} path="*" />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <div className="text-purple-600 flex justify-between min-w-2xl pb-10">
+      <div className="flex gap-10">
+        {" "}
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+      <Link to="/login">Login</Link>
+    </div>
+  );
+};
