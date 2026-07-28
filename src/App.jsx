4790438c3ad3cbdc153383e3337bedbdcc8c8505
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-27 23:56:55 -0700

    initial commit on the openvo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [name, setName] = useState("");
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Name..."
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      />{" "}
+      <h1>Sign Up</h1>
     </div>
   );
 };
