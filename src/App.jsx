6d2d585eb8cea629074f3c84cd32f0c949154a72
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-27 23:57:37 -0700

    initial commit on the openvo app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ const App = () => {
   return (
     <div>
       <h1>Sign Up</h1>
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" />
+      </div>{" "}
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" />
+      </div>
+      <button>Create Account</button>
     </div>
   );
 };
