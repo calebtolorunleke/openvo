143e86a2f2f0fecc962079605cd3b9a7bcd6f2de
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-28 19:25:08 -0700

    Implement authentication with React Context API

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../AuthContext";
 
 export const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
+
+  const hanldeLogout = ({ name }) => {
+    logout(name);
+  };
   return (
     <div className="text-purple-600 flex justify-between min-w-2xl pb-10">
       <div className="flex gap-10">
@@ -11,7 +15,8 @@ export const Navbar = () => {
         <Link to="/">Home</Link>
         <Link to="/about">Profile</Link>
       </div>
-      <Link to="/login">{user.isAuth ? "Logout" : "Login"}</Link>
+
+      {user.isAuth ? <button>Logout</button> : <Link to="/login">Login</Link>}
     </div>
   );
 };
