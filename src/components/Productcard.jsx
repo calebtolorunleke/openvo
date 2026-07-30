f5515ce280878b2d88bd4381f195b4d535941182
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-30 01:13:08 -0700

    Build product card UI and improve styling

---
+++ src/components/Productcard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Productcard = ({ product }) => {
+  return (
+    <div className="rounded-xl overflow-hidden bg-white shadow-sm hover:shadow-md transition-all duration-300 hover:-translate-y-2">
+      <img src={product.image} alt="" className="w-full h-50 object-cover " />
+      <div className="py-7 bg-white px-3">
+        <h3 className="text-gray-600 font-semibold pb-2 line-clamp-2 overflow h-12 overflow-hidden">
+          {product.name}
+        </h3>
+        <p className="text-blue-500 font-semibold pb-2">$ {product.price}</p>
+        <div className="flex gap-2 text-white">
+          <Link className="bg-gray-400 px-3 text-xs py-2 rounded">
+            View Details
+          </Link>
+          <button className="bg-blue-400 px-3 py-2 text-xs rounded">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Productcard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { products } from "../data/products";
-import { Link } from "react-router-dom";
+import Productcard from "../components/Productcard";
 
 const Home = () => {
   return (
@@ -21,31 +21,9 @@ const Home = () => {
           Our products
         </h2>
 
-        <div className="grid grid-cols-3 gap-5">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-7">
           {products.map((product) => (
-            <div className="rounded-xl overflow-hidden" key={product.id}>
-              <img
-                src={product.image}
-                alt=""
-                className="w-full h-50 object-cover "
-              />
-              <div className="py-7 bg-white px-3">
-                <h3 className="text-gray-600 font-semibold pb-2">
-                  {product.name}
-                </h3>
-                <p className="text-blue-500 font-semibold pb-2">
-                  $ {product.price}
-                </p>
-                <div className="flex gap-2 text-white">
-                  <Link className="bg-gray-400 px-3 text-xs py-2 rounded">
-                    View Details
-                  </Link>
-                  <button className="bg-blue-400 px-3 py-2 text-xs rounded">
-                    Add to Cart
-                  </button>
-                </div>
-              </div>
-            </div>
+            <Productcard key={product.id} product={product} />
           ))}
         </div>
       </div>
