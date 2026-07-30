c2c6f9260414ba32830c3ecf2cf3ab191dc9ac03
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-29 23:10:23 -0700

    hero initial commit push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto py-20 px-12 flex items-center justify-center ">
+        <div className="text-center">
+          {" "}
+          <h1 className="text-4xl font-bold leading-tight tracking-wider pb-5">
+            Welcome to ShopHub
+          </h1>
+          <p className="text-gray-600 text-lg">
+            Discover amazing products and great prices
+          </p>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Home;
