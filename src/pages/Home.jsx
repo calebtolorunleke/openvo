4557c1aecf1e808f393645ed3f578f953debdaba
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-30 00:56:16 -0700

    Build product card UI and improve styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { products } from "../data/products";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -15,9 +17,37 @@ const Home = () => {
         </div>
       </div>
       <div className="px-12">
-        <h2 className="text-2xl font-semibold text-gray-600 tracking-wide">
+        <h2 className="text-2xl font-semibold text-gray-600 tracking-wide py-5">
           Our products
         </h2>
+
+        <div className="grid grid-cols-3 gap-5">
+          {products.map((product) => (
+            <div className="rounded-xl overflow-hidden" key={product.id}>
+              <img
+                src={product.image}
+                alt=""
+                className="w-full h-50 object-cover "
+              />
+              <div className="py-7 bg-white px-3">
+                <h3 className="text-gray-600 font-semibold pb-2">
+                  {product.name}
+                </h3>
+                <p className="text-blue-500 font-semibold pb-2">
+                  $ {product.price}
+                </p>
+                <div className="flex gap-2 text-white">
+                  <Link className="bg-gray-400 px-3 text-xs py-2 rounded">
+                    View Details
+                  </Link>
+                  <button className="bg-blue-400 px-3 py-2 text-xs rounded">
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
