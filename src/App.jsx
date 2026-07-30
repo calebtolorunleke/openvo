53ca9dc543837bc159311626a7758e61f379ff29
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-29 22:29:45 -0700

    Implement authentication with React Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,57 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Login from "./components/Login";
-import Notfound from "./components/Notfound";
-import { Navbar } from "./components/Navbar";
-import { AuthContext } from "./AuthContext";
-import { useState } from "react";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import Login from "./components/Login";
+// import Notfound from "./components/Notfound";
+// import { Navbar } from "./components/Navbar";
+// import { AuthContext } from "./AuthContext";
+// import { useState } from "react";
+
+// const App = () => {
+//   const [user, setUser] = useState({ name: "", isAuth: false });
+
+//   const login = (name) => {
+//     setUser({ name: name, isAuth: true });
+//   };
+
+//   const logout = (name) => {
+//     setUser({ name: "", isAuth: false });
+//   };
+
+//   return (
+//     <BrowserRouter>
+//       <AuthContext.Provider value={{ user, login, logout }}>
+//         <Navbar />{" "}
+//         <Routes>
+//           <Route element={<Home />} path="/" />
+//           <Route element={<About />} path="/about" />
+//           <Route element={<Login />} path="/login" />
+//           <Route element={<Notfound />} path="*" />
+//         </Routes>
+//       </AuthContext.Provider>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+import React, { useEffect } from "react";
 
 const App = () => {
-  const [user, setUser] = useState({ name: "", isAuth: false });
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users",
+        );
+        const data = response.json();
+        console.log(data);
+      } catch (error) {}
+    };
+    fetchUser();
+  }, []);
 
-  const login = (name) => {
-    setUser({ name: name, isAuth: true });
-  };
-
-  const logout = (name) => {
-    setUser({ name: "", isAuth: false });
-  };
-
-  return (
-    <BrowserRouter>
-      <AuthContext.Provider value={{ user, login, logout }}>
-        <Navbar />{" "}
-        <Routes>
-          <Route element={<Home />} path="/" />
-          <Route element={<About />} path="/about" />
-          <Route element={<Login />} path="/login" />
-          <Route element={<Notfound />} path="*" />
-        </Routes>
-      </AuthContext.Provider>
-    </BrowserRouter>
-  );
+  return <div>App</div>;
 };
 
 export default App;
