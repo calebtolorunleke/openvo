add594242edd20e9dc22c5882732492d66fbfc9b
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-29 23:13:46 -0700

    hero initial commit push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <div className="max-w-7xl mx-auto py-20 px-12 flex items-center justify-center ">
+    <div className="max-w-7xl mx-auto">
+      <div className=" py-20 px-12 flex items-center justify-center ">
         <div className="text-center">
           {" "}
           <h1 className="text-4xl font-bold leading-tight tracking-wider pb-5">
@@ -14,7 +14,11 @@ const Home = () => {
           </p>
         </div>
       </div>
-      <div></div>
+      <div className="px-12">
+        <h2 className="text-2xl font-semibold text-gray-600 tracking-wide">
+          Our products
+        </h2>
+      </div>
     </div>
   );
 };
