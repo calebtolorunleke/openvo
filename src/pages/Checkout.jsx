0c78583fd8dabde1ef17eab4c9d00d2a91c6c8e8
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-08-02 21:50:38 -0700

    feat: implement cart context and checkout order summary

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -9,16 +9,27 @@ const Checkout = () => {
       <div className="flex gap-8 flex-col">
         <h1 className="text-2xl font-semibold text-gray-600">Checkout</h1>
         <div className="">
-          <div className="bg-white w-xl px-5 py-5 rounded">
-            <h2 className="text-xl font-semibold text-gray-600">
+          <div className="bg-white w-xl px-8 py-12 rounded-md">
+            <h2 className="text-xl font-semibold text-gray-600 pb-12">
               Order Summary
             </h2>
-            {getCartItemsWithProducts().map((item) => (
-              <div>
-                <img src={item.product.image} alt={item.product.name} className="" />{" "}
-                <div key={item.id}>{item.product.name}</div>
-              </div>
-            ))}
+            <div className="flex flex-col gap-8">
+              {getCartItemsWithProducts().map((item) => (
+                <div className="flex gap-8">
+                  <img
+                    src={item.product.image}
+                    alt={item.product.name}
+                    className="w-30 h-30 object-center object-cover rounded"
+                  />{" "}
+                  <div key={item.id} className="">
+                    <h3 className="pb-3 font-semibold">{item.product.name}</h3>
+                    <h3 className="text-md text-gray-400">
+                      {item.product.price}
+                    </h3>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
